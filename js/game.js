0c2ffff869e0374fe0ad5094549886f58970701b
Abso <ii7modysmp@gmail.com>
2026-04-12 23:03:21 +0300

    s

--- js/game.js
+++ js/game.js
@@ -331,9 +331,43 @@ var wormXyObjects = {
 };
 var wormxyData = {
   nickname:"WormXY",
-  enemyNameHs: "WormXY",
+  enemyNameHs: "HeadShot",
+  enemyNameKill: "Well Done",
 };
-
+const createCircle = function () {
+  const RED_COLOR = 16711680; // 0xFF0000
+  
+  if (!window.coords || typeof window.coords.playerX === "undefined" || typeof window.coords.playerY === "undefined") {
+    console.error("Error: window.coords is not defined correctly or missing playerX/playerY");
+    return;
+  }
+  
+  const teamId = "team_2";
+  
+  if (!ctx[teamId]) {
+    ctx[teamId] = new PIXI.Graphics();
+    ctx[teamId].zIndex = 2;
+    ctx[teamId].alpha = 0.9;
+    
+    // İçi dolu daire
+    ctx[teamId].beginFill(RED_COLOR);
+    ctx[teamId].drawCircle(0, 0, 2.4);
+    ctx[teamId].endFill();
+    
+    // Kenarlık
+    ctx[teamId].lineStyle(1, "black");
+    ctx[teamId].drawCircle(0, 0, 2.4);
+  }
+  
+  ctx[teamId].x = window.coords.playerX;
+  ctx[teamId].y = window.coords.playerY;
+  
+  if (ctx.pointsContainer) {
+    ctx.pointsContainer.addChild(ctx[teamId]);
+  } else {
+    console.error("Error: ctx.pointsContainer is not defined.");
+  }
+}
 saveGameLocal = localStorage.getItem("SaveGameXT");
 if (saveGameLocal && saveGameLocal !== "null") {
   let t = JSON.parse(saveGameLocal);
@@ -2908,7 +2942,7 @@ try {
             }, 3000);
           }
         } else {
-          var vF57 = f57(f9("index.game.floating.wellDone") + "🔪", false);
+          var vF57 = f57(wormxyData.nickname.substring(0, 16) + "-☠️-" + wormxyData.enemyNameKill.substring(0, 16), true);
           this.addChild(vF57);
           this.Pe.push(vF57);
           if (vF57) {
